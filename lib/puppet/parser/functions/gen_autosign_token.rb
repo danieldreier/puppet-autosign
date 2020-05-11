a104dec3003480593e80d685a0308f03cd289a28
Corey Osman <corey@nwops.io>
2020-05-11 16:31:08 -0700

    Fix rubocop errors

--- lib/puppet/parser/functions/gen_autosign_token.rb
+++ lib/puppet/parser/functions/gen_autosign_token.rb
@@ -3,7 +3,7 @@ require 'socket'
 require 'logging'
 
 module Puppet::Parser::Functions
-  newfunction(:gen_autosign_token, :type => :rvalue, :doc => <<-EOS
+  newfunction(:gen_autosign_token, type: :rvalue, doc: <<-EOS
     Generate a JWT autosign token for use with the autosign gem's
     autosign policy executable.
 
@@ -11,20 +11,20 @@ module Puppet::Parser::Functions
     used to sign the token, and other settings are determined by settings in
     autosign.conf.
     EOS
-  ) do |arguments|
+             ) do |arguments|
     @logger = Logging.logger['Autosign']
     @logger.level = :info
     @logger.add_appenders Logging.appenders.stdout
 
-    config = Autosign::Config.new()
+    config = Autosign::Config.new
     case arguments.size
     when 1
       jwt_token_validity = config.settings['jwt_token']['validity']
     when 2
-      raise(Puppet::ParseError, "gen_autosign_token(): second argument must be a positive integer") unless arguments[1].to_i > 0
+      raise(Puppet::ParseError, 'gen_autosign_token(): second argument must be a positive integer') unless arguments[1].to_i > 0
       jwt_token_validity = arguments[1].to_i
     else
-      raise(Puppet::ParseError, "gen_autosign_token(): Wrong number of arguments " +
+      raise(Puppet::ParseError, 'gen_autosign_token(): Wrong number of arguments ' \
       "given (#{arguments.size} for 1 or 2)")
     end
 
@@ -32,13 +32,13 @@ module Puppet::Parser::Functions
     jwt_secret = config.settings['jwt_token']['secret'] unless config.settings['jwt_token']['secret'].nil?
 
     if jwt_secret.nil?
-      raise(Puppet::ParseError, "gen_autosign_token(): cannot generate token. " +
-            "No secret provided in /etc/autosign.conf or JWT_TOKEN_SECRET env variable")
+      raise(Puppet::ParseError, 'gen_autosign_token(): cannot generate token. ' \
+            'No secret provided in /etc/autosign.conf or JWT_TOKEN_SECRET env variable')
     end
 
     token = Autosign::Token.new(arguments[0].to_s, false, jwt_token_validity.to_i, Socket.gethostname.to_s, jwt_secret)
 
-    #value = function_str2bool([arguments[0]])
+    # value = function_str2bool([arguments[0]])
 
     # We have real boolean values as well ...
     result = token.sign
--- spec/classes/class_spec.rb
+++ spec/classes/class_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-shared_examples_for "base case" do
+shared_examples_for 'base case' do
   it { is_expected.to compile.with_all_deps }
 
   it { is_expected.to contain_class('autosign::params') }
@@ -9,26 +9,29 @@ shared_examples_for "base case" do
 end
 
 describe 'autosign' do
-  context "autosign class without any parameters" do
+  context 'autosign class without any parameters' do
     on_supported_os.each do |os, os_facts|
       context "on #{os}" do
         let(:facts) { os_facts }
-        let(:params) {{ }}
+        let(:params) { {} }
 
-        it_behaves_like "base case"
+        it_behaves_like 'base case'
         it { is_expected.to contain_package('autosign').with_ensure('present') }
       end
     end
   end
 
-  context "autosign class with some parameters" do
+  context 'autosign class with some parameters' do
     on_supported_os.each do |os, os_facts|
       context "on #{os}" do
         let(:facts) { os_facts }
-        let(:params) {{
-          :ensure => 'latest',
-          :config => { 'jwt_token' => { 'secret' => 'hunter2' } },
-          }}
+        let(:params) do
+          {
+            ensure: 'latest',
+            config: { 'jwt_token' => { 'secret' => 'hunter2' } },
+          }
+        end
+
         if ['FreeBSD', 'OpenBSD'].include?(os_facts[:osfamily])
           base_configpath = '/usr/local/etc'
           base_journalpath = '/var/autosign'
@@ -37,7 +40,7 @@ describe 'autosign' do
           base_journalpath = '/var/lib/autosign'
         end
 
-        it_behaves_like "base case"
+        it_behaves_like 'base case'
 
         it { is_expected.to contain_package('autosign').with_ensure('latest') }
         it { is_expected.to contain_file("#{base_configpath}/autosign.conf").with_ensure('file') }
@@ -50,24 +53,26 @@ describe 'autosign' do
 
   context 'unsupported operating system' do
     describe 'autosign class without any parameters on Solaris/Nexenta' do
-      let(:facts) {{
-        :osfamily        => 'Solaris',
-        :operatingsystem => 'Nexenta',
-        }}
+      let(:facts) do
+        {
+          osfamily: 'Solaris',
+          operatingsystem: 'Nexenta',
+        }
+      end
 
-      it { expect { is_expected.to contain_package('autosign') }.to raise_error(Puppet::Error, /Nexenta not supported/) }
+      it { expect { is_expected.to contain_package('autosign') }.to raise_error(Puppet::Error, %r{Nexenta not supported}) }
     end
   end
 
-  context "when running Puppet Enterprise" do
+  context 'when running Puppet Enterprise' do
     on_supported_os.each do |os, os_facts|
       context "on #{os}" do
-        let(:facts) { os_facts.merge({
-          :pe_server_version => '2017.3.2',
-          }) }
-        let(:params) {{ }}
+        let(:facts) do
+          os_facts.merge(pe_server_version: '2017.3.2')
+        end
+        let(:params) { {} }
 
-        it_behaves_like "base case"
+        it_behaves_like 'base case'
         it { is_expected.to contain_package('autosign').with_ensure('present') }
         it { is_expected.to contain_file('/var/log/puppetlabs/puppetserver/autosign.log').with_ensure('file') }
         it { is_expected.to contain_file('/etc/puppetlabs/puppetserver/autosign.conf').with_ensure('file') }
@@ -77,34 +82,38 @@ describe 'autosign' do
     end
   end
 
-  context "when overriding options" do
+  context 'when overriding options' do
     on_supported_os.each do |os, os_facts|
       context "on #{os}" do
         let(:facts) { os_facts }
-        let(:params) {{
-          :ensure             => '0.1.0',
-          :configfile         => '/etc/autosign1.conf',
-          :manage_journalfile => false,
-          :manage_logfile     => false,
-        }}
+        let(:params) do
+          {
+            ensure: '0.1.0',
+            configfile: '/etc/autosign1.conf',
+            manage_journalfile: false,
+            manage_logfile: false,
+          }
+        end
 
-        it_behaves_like "base case"
+        it_behaves_like 'base case'
         it { is_expected.to contain_package('autosign').with_ensure('0.1.0') }
         it { is_expected.to contain_file('/etc/autosign1.conf').with_ensure('file') }
-        it { is_expected.not_to contain_file('/var/lib/autosign/autosign.journal')}
-        it { is_expected.not_to contain_file('/var/log/autosign.log')}
+        it { is_expected.not_to contain_file('/var/lib/autosign/autosign.journal') }
+        it { is_expected.not_to contain_file('/var/log/autosign.log') }
         it { is_expected.not_to contain_file('/var/lib/autosign') }
       end
     end
   end
 
-  context "autosign class with manage_package=false" do
+  context 'autosign class with manage_package=false' do
     on_supported_os.each do |os, os_facts|
       context "on #{os}" do
         let(:facts) { os_facts }
-        let(:params) {{
-          :manage_package => false,
-        }}
+        let(:params) do
+          {
+            manage_package: false,
+          }
+        end
 
         it { is_expected.not_to contain_package('autosign') }
       end
